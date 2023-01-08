5d95e82d10acbc077b7dde45d0262b89edf76c8b
99nighty <117559858+99nighty@users.noreply.github.com>
2023-01-09 00:15:45 +0900

    최신화
    최신화

--- src/main/webapp/js/google_mainmap.js
+++ src/main/webapp/js/google_mainmap.js
@@ -1,13 +1,13 @@
-   var map;
-   function initMap(){
-      var les={lat:37.500600, lng:127.036268};
-      map=new google.maps.Map(
-         document.getElementById('map'),
-         {zoom:16,center:les}
-      );
-      
-      new google.maps.Marker(
-         {position:les, map:map, label:"즐겁고 신나는곳"}
-      );
-   }
-   initMap();
+var map;
+function initMap(){
+   var les={lat:37.500600, lng:127.036268};
+   map=new google.maps.Map(
+      document.getElementById('map'),
+      {zoom:16,center:les}
+   );
+   
+   new google.maps.Marker(
+      {position:les, map:map, label:"즐겁고 신나는곳"}
+   );
+}
+initMap();
