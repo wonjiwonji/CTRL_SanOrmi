cadd545077dfe7edb379f43c2296bf96ec347153
LeeMin9yeon9 <122258997+LeeMin9yeon9@users.noreply.github.com>
2023-01-25 10:46:58 +0900

    쿼리문
    뿜뿜

---
+++ ERD/sample.sql
@@ -0,0 +1,11 @@
+-- Member
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('mingyeong','1234', '이민경', '', 'dlalsrud0624@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('jiwon','1111', '강지원', '', 'dnflwlq1214@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('jeongyeon','2222', '허정연', '별내3로', 'qazseeszaq3219@gmail.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('jaehwan','3333', '이재환', '', 'never37@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('dlalsrud@naver.com','NAVER', '이민경', '', 'dlalsrud@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('dnflwlq@naver.com','KAKAO', '강지원', '', 'dnflwlq@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('hyerin','4444', '인혜린', '테헤란로 26길 12', 'hyerin0508@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('dlwjddk','5555', '이정아', '', 'dlwjddk@naver.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('hanbit@kakao.com','KAKAO', '김한빛', '', 'hanbit@kakao.com', NOW());
+INSERT INTO MEMBER (ID, M_PW, M_NAME, M_ADDRESS , M_EMAIL ,M_REGDATE) VALUES ('jaeho','6666', '이재호', '', 'jaeho@naver.com', NOW());
