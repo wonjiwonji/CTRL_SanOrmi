3ad609dda5b00393f3ead02b0292bfaa90307dc6
99nighty <117559858+99nighty@users.noreply.github.com>
2023-01-25 09:18:06 +0900

    1
    1

---
+++ src/main/java/controller/QViewAction.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class QViewAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward=new ActionForward();
+		forward.setPath("/view.jsp");
+		forward.setRedirect(false);
+		
+		
+		
+		return forward;
+	}
+
+}
